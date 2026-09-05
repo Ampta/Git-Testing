5d40262f534ac6d3673be602c71ea1c6d7d04233
Shivam Gupta <ampta77000@gmail.com>
2026-09-05 19:28:41 +0530

    demo startup added

--- src/main/java/com/ampta/teamgit/Demo.java
+++ src/main/java/com/ampta/teamgit/Demo.java
@@ -1,7 +1,25 @@
 package com.ampta.teamgit;
 
+import com.ampta.teamgit.entity.Order;
+import com.ampta.teamgit.entity.Product;
+
 public class Demo {
     public static void main(String[] args) {
+        Order order = new Order();
+        order.setId(1);
+        order.setName("PRO");
+        order.setPrice(200.22);
+
+        System.out.println(order.toString());
+
+        Product product = new Product();
+
+        product.setId(1L);
+        product.setName("slkdfj");
+        product.setPrice(23);
+
+        System.out.println(product.toString());
+
         System.out.println("Working");
     }
 }
